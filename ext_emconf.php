14097126fcd278f3e70c08c874c267e86e5fc4c3
eike <eike@hal>
2018-11-24 14:04:56 +0100

    [TASK] release version 2.0.0 for typo3 7.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '0.0.1',
+	'version' => '2.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2',
+			'typo3' => '7.6',
 		),
 		'conflicts' => array(
 		),
