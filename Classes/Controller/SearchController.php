3e2612ef68a00f6b333ddbfaf6f1652d01a46ba7
eike.starkmann <eike.starkmann@undkonsorten.com>
2019-11-05 12:08:01 +0100

    [BUGFIX] dont allow system relevant form manipulation

--- Classes/Controller/SearchController.php
+++ Classes/Controller/SearchController.php
@@ -86,6 +86,12 @@ class SearchController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
      */
     public function searchAction(Demand $demand, $page = 1)
     {
+        if ($this->settings['domain']&&$this->settings['port']) {
+            $demand->setDomain($this->settings['domain']);
+            $demand->setPort($this->settings['port']);
+            $demand->setInterface($this->settings['interface']);
+            $demand->setProtocol($this->settings['protocol']);
+        }
         /** @var SearchRepositoryFactory $searchRepositoryFactory */
         $searchRepositoryFactory = $this->objectManager->get(SearchRepositoryFactory::class);
         $this->searchRepository = $searchRepositoryFactory->createSearchRepository($demand->getInterface());
