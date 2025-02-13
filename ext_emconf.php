6736d4205c7851c3127565f8780aa98caefe3714
Eike Starkmann <eike.starkmann@posteo.de>
2025-02-13 18:26:17 +0100

    [TASK] increase version to 5.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,10 +17,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Eike Starkmann',
     'author_email' => 'eikestarkmann@posteo.de',
     'state' => 'stable',
-    'version' => '5.0.0',
+    'version' => '5.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [
         ],
